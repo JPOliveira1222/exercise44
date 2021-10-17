2ea7a7b75f0505cbbbc232fbb2b39e454e99f669
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-16 20:07:49 -0400

    pseudocode for exercise 44

---
+++ src/main/java/baseline/solution44.java
@@ -0,0 +1,35 @@
+package baseline;
+
+public class solution44 {
+
+    /*
+    add an external library
+    create a parser object
+
+    read the file and store it in an object.
+    create an object to read internal values
+
+    read the products array from the file
+    create reader object that takes user input
+
+    create a string variable
+    use a flag to see if a string is present or not
+
+    print "What is the product name? "
+    input = user input
+
+    created iterator for an array for the products
+    using a while loop, loop through the iterator
+    created another object
+
+    use if to make sure the string value isn't flagged.
+    make sure the input is read as upper or lower case
+    print "Name: " +name
+    print "Price: "+price gotten from iterator
+    print "Quantity: "+quantity gotten from iterator
+
+    if it is flagged = 0
+    print "sorry, the product was not found in the inventory"
+
+     */
+}
